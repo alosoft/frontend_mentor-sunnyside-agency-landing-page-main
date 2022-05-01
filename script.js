5bbd37d96031be851363f4545917819d611c08e4
Raymond <raymondadutwum.agyei@softwaregroup.com>
2022-05-01 22:53:23 +0000

    update

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
-const yellow_link = document.querySelector('.yellow');
-const red_link = document.querySelector('.red');
-const yellow_line = document.querySelector('.yellow_line');
-const red_line = document.querySelector('.red_line');
 const humburger_menu = document.querySelector('.humburger_menu');
 const overlay = document.querySelector('.overlay');
-const mobile__menu = document.querySelector('.mobile__menu');
 
 humburger_menu.addEventListener('click', () => {
-    overlay.style.display = 'block';
+    if (overlay.style.display === 'block') {
+        overlay.style.display = 'none';
+    } else {   
+        overlay.style.display = 'block';
+        humburger_menu.src = './images/close.svg'
+    }
+
 })
 
 overlay.addEventListener('click', () => {
     overlay.style.display = 'none';
-})
-
-yellow_link.addEventListener('mouseover', () => {
-    yellow_line.style.display = 'block';
-})
-
-yellow_link.addEventListener('mouseleave', () => {
-    yellow_line.style.display = 'none';
-})
-
-red_link.addEventListener('mouseover', () => {
-    red_line.style.display = 'block';
-})
-
-red_link.addEventListener('mouseleave', () => {
-    red_line.style.display = 'none';
+    humburger_menu.src = './images/menu.svg'
 })
